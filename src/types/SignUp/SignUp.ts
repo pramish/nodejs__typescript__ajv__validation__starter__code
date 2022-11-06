8d5a7fa672df72afacc540e7045035b4ed59ab4c
Pramish Luitel <pramish.luitel7@gmail.com>
2022-11-06 13:44:30 +1100

    Update SignUp.ts

--- src/types/SignUp/SignUp.ts
+++ src/types/SignUp/SignUp.ts
@@ -1,5 +1,11 @@
 import { IResponse } from "../Response";
 
+export interface ISignUp {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 export interface ISignUpResponse extends IResponse {
   firstName?: string;
   lastName?: string;
